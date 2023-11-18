7badf3e477aff8b0055ce89b2aab9c1342983889
Chesnokov Arkady <arkady-chesnokof@yandex.ru>
2023-11-18 23:38:37 +0300

    remove unused import

--- src/main/java/com/cleanyco/s6ch/data/User.java
+++ src/main/java/com/cleanyco/s6ch/data/User.java
@@ -3,7 +3,6 @@ package com.cleanyco.s6ch.data;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.NonNull;
 
