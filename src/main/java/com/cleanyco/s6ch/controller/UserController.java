9d7ba3783e54e9a9c337805cfc3c336ca06f16b6
Chesnokov Arkady <arkady-chesnokof@yandex.ru>
2023-11-19 15:47:47 +0300

    create UserDTO & add mapToDTO() in UserService & change return value to ResponseEntity<UserDTO> in getUserByUsername in UserController

--- src/main/java/com/cleanyco/s6ch/controller/UserController.java
+++ src/main/java/com/cleanyco/s6ch/controller/UserController.java
@@ -1,13 +1,12 @@
 package com.cleanyco.s6ch.controller;
 
 import com.cleanyco.s6ch.model.User;
+import com.cleanyco.s6ch.payload.UserDTO;
 import com.cleanyco.s6ch.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Optional;
 
 @RestController
 public class UserController {
@@ -19,12 +18,10 @@ public class UserController {
     }
 
     @GetMapping("/users/{username}")
-    public ResponseEntity<User> getUserByUsername(@PathVariable String username) {
-        Optional<User> user = userService.getUserByUsername(username);
-
-        return user.map(value -> new ResponseEntity<>(value, HttpStatus.OK))
+    public ResponseEntity<UserDTO> getUserByUsername(@PathVariable String username) {
+        return userService.getUserByUsername(username)
+                .map(user -> new ResponseEntity<>(userService.mapToDTO(user), HttpStatus.OK))
                 .orElseGet(() -> new ResponseEntity<>(HttpStatus.BAD_REQUEST));
-
     }
 
     //TODO add hashing!
--- src/main/java/com/cleanyco/s6ch/service/UserService.java
+++ src/main/java/com/cleanyco/s6ch/service/UserService.java
@@ -1,6 +1,7 @@
 package com.cleanyco.s6ch.service;
 
 import com.cleanyco.s6ch.model.User;
+import com.cleanyco.s6ch.payload.UserDTO;
 import com.cleanyco.s6ch.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -26,5 +27,9 @@ public class UserService {
 
     public void saveUser(User user) {
         userRepository.save(user);
+    }
+
+    public UserDTO mapToDTO(User user) {
+        return new UserDTO(user.getUsername(), user.getCreatedAt(), user.getUpdatedAt());
     }
 }
